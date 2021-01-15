c60d635a5fb88ad65a8b07a587b35c1fec2cf925
EliasNesheim <elias.nesheim@gmail.com>
2021-01-15 16:41:49 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path = '/' exact />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home(){
+    return(
+        <main>
+            <section>
+                <h1> Hei! Mitt navn er Elias Christopher Nesheim.</h1>
+            </section>
+        </main>
+        )
+}
