363bd45f8173b440b726aea92cd2f94fc1834595
Sohel Samimi <47162719+SohelSamim@users.noreply.github.com>
2021-02-05 04:15:15 +0100

    Navbar
    Build the navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Spørreskjema',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: '',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
