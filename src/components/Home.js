503020ae914a347bb130edc8532b5659bc2c7c23
OK12032019 <48478599+OK12032019@users.noreply.github.com>
2021-02-10 14:34:02 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,16 +33,48 @@ export default function Home(){
     
 
     return(
-        <main>
-            <section>
-                <h1> Bærekraftsmotor</h1>
-                <h2> Piotr Marek Kramer </h2>
-                <h3>Øyvind</h3>
-                <section>
-                    <h1>respons fra get request fra server:</h1>
-                    <h2>{postData}</h2>
-                </section>
-            </section>
-        </main>
+<head>
+<meta charset="utf-8"> <!--Aksepterer ÆØÅ-->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!--Mobil skjermer-->
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"><!--version 4.1----> <!--ER CSS'en-->
+<!--<link rel="stylesheet" href="pages.css"> -->
+
+	<title>Bærekraftsmotor </title> <!--Tittel-->
+</head>
+<body>
+  <nav class> <!--Navbaren-->
+        <div class="nav-wrapper">
+            <a href="pages2.html" class="brand-logo"><img src="Logos.png" alt="Logoen" style="width:48px;"><img></a>
+            <a href="javascript:void(0);" onclick="myFunction()" data-target="mobile-demo" class="top-nav sidenav-trigger hide-on-large-only   "><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+
+                <li><a href="melding.php">Spørreskjema</a></li> <!--Link-->
+                <li><a href="regel.php">Om Simpact</a></li>
+                <li><a href="ny.php">Kontakt oss</a></li>
+                <li><a class="waves-effect waves-light btn-large" href="log.php">Brukerside</a></li>
+            </ul>
+        </div>
+    </nav> 
+
+<div class="jumbotron"> <!--CSS-->
+  <h1>Bruk Bærekraftsmotor</h1> <!--Overskrift-->
+  <p class="">Bærekraftsmotor er en nettside som spør deg eller din bedrift en rekke spørsmål om hvor godt dere følger klima målene og kommer med råd for videre utvikling </p> <!--Tekst-->
+  <hr class="">
+  <img src="Bilde1.png" alt="bilde av noter"> <!--Bilde-->
+  <br>
+  <br>
+  
+  <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal" style="right: 50%;"> <!--Mulighet gå tilbake til Simpact.no-->
+Gå tilbake til simpact.no
+</button> 
+
+<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal"> <!--Mulighet til å gå til spørreskjema-->
+Gå til spørreskjema
+</button>
+ </div>
+</body>
+
+</html>
         )
 }
