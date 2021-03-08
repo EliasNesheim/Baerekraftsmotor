fcd10aa1cde33bf9343df215f3530a1a2ef60eb8
EliasNesheim <elias.nesheim@gmail.com>
2021-03-08 18:33:25 +0100

    kommentarer

--- src/components/PageOneAndAHalf.js
+++ src/components/PageOneAndAHalf.js
@@ -6,12 +6,6 @@ import Question from "./Question";
 import { motion } from 'framer-motion';
 
 
-
-// å lage en ny useState for hver gang vi instansierer en Question componen er ikke riktig måte å gjøre dette på, but here we are
-// https://www.youtube.com/watch?v=k238XpMMn38
-
-
-
 export default function PageOneAndAHalf({ appState, setAppState, Answers, setAnswers, AnswerKey, setAnswerKey}){
 
 
@@ -21,7 +15,9 @@ export default function PageOneAndAHalf({ appState, setAppState, Answers, setAns
             <TilbakeKnapp plusMinus={true} appState={appState} setAppState={setAppState} />
 
             <div>
-                
+                {
+                    //instansier spørsmål komponenten og inkrementerer AnswerKey mellom hvergang, variabler kommer fra app.js
+                }
                 <Question QType={"Skala"} QText={"I hvilken grad er bedriftens bærekraftsarbeids strategisk viktig?"} AnswerKey={AnswerKey} setAnswerKey={setAnswerKey} Answers={Answers} setAnswers={setAnswers} />
                 {AnswerKey = AnswerKey + 1 }
                 <Question QType={"Skala"} QText={"lol2k?"} AnswerKey={AnswerKey} setAnswerKey={setAnswerKey} Answers={Answers} setAnswers={setAnswers} />
